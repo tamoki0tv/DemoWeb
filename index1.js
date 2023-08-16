149a6fef3207a9e1ebff75c62ce381ba30f18339
tamoki0tv <tvloi250204@gmail.com>
2023-08-16 21:18:57 +0700

    demo4

--- index1.js
+++ index1.js
@@ -18,6 +18,15 @@ window.addEventListener('scroll', function() {
   
 });
 
+const menuIcon = document.querySelector('.menu-icon');
+const leftMenu = document.querySelector('.left');
+const rightMenu = document.querySelector('.right');
+
+menuIcon.addEventListener('click', function () {
+    leftMenu.classList.toggle('active'); // Hiển thị hoặc ẩn menu trái
+    rightMenu.classList.toggle('active'); // Hiển thị hoặc ẩn menu phải
+});
+
 // Lấy thông tin user từ tệp JSON hoặc cơ sở dữ liệu
 const userJSON = localStorage.getItem('user');
 const user = JSON.parse(userJSON);
@@ -33,4 +42,37 @@ if (user) {
     <p><strong>Username:</strong> ${userUsername}</p>
     <p><strong>Phone:</strong> ${userPhone}</p>
   `;
+}
+
+
+function loadItem() {
+  fetch("item-uudai.json").then(res => res.json()).then(data => {
+    let h = "";
+    for (let p of data)
+      h+=`
+                            <div class="item">
+                                <a href="#">
+                                    <div class="img-item">
+                                        <img src="${p.img}">
+                                    </div>
+                                    <div class="info-item">
+                                        <div class="item-address">
+                                            <h4>${p.name}</h4>
+                                            <div class="address">Giá ${p.price}</div>
+                                        </div>
+                                        <p class="type">
+                                            <i class="fa-solid fa-tag"></i>
+                                            Giảm ${p.type}
+                                        </p>
+                                    </div>
+                                </a>
+                            </div>
+                                `;
+    let e = document.getElementById("list-item");
+    e.innerHTML = h;
+    
+  })
+}
+window.onload = () => {
+  loadItem();
 }
