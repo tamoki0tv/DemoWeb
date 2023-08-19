5d15fcf78f20727b07c88f96c06d3588a485f692
tamoki0tv <tvloi250204@gmail.com>
2023-08-19 20:36:47 +0700

    demo6

--- index1.js
+++ index1.js
@@ -1,31 +1,29 @@
 const searchElement = document.getElementById('fix');
-const discountsElement = document.getElementById('container2');
+const searchElement1 = document.getElementById('fix-img');
 
 window.addEventListener('scroll', function() {
   const scrollY = window.scrollY;
   
-  if (scrollY >= 400 && scrollY <1100) {
+  if (scrollY >= 420 && scrollY <1250) {
     searchElement.classList.remove('ctn-absolute');
     searchElement.classList.add('ctn-fixed');
-  } else if (scrollY >= 1100) {
+    searchElement1.classList.remove('ctn-absolute');
+    searchElement1.classList.add('ctn-fixed');
+  } else if (scrollY >= 1250) {
     searchElement.classList.remove('ctn-fixed');
     searchElement.classList.add('ctn-absolute');
+    searchElement1.classList.remove('ctn-fixed');
+    searchElement1.classList.add('ctn-absolute');
   } else {
     searchElement.classList.remove('ctn-absolute');
     searchElement.classList.remove('ctn-fixed');
+    searchElement1.classList.remove('ctn-absolute');
+    searchElement1.classList.remove('ctn-fixed');
   }
   
   
 });
 
-const menuIcon = document.querySelector('.menu-icon');
-const leftMenu = document.querySelector('.left');
-const rightMenu = document.querySelector('.right');
-
-menuIcon.addEventListener('click', function () {
-    leftMenu.classList.toggle('active'); // Hiển thị hoặc ẩn menu trái
-    rightMenu.classList.toggle('active'); // Hiển thị hoặc ẩn menu phải
-});
 
 // Lấy thông tin user từ tệp JSON hoặc cơ sở dữ liệu
 const userJSON = localStorage.getItem('user');
@@ -38,9 +36,9 @@ if (user) {
 
   const userInfoDiv = document.getElementById('userInfo');
   userInfoDiv.innerHTML = `
-    <p><strong>Full Name:</strong> ${userFullName}</p>
-    <p><strong>Username:</strong> ${userUsername}</p>
-    <p><strong>Phone:</strong> ${userPhone}</p>
+    <p><strong style="color: black;">Full Name:</strong> ${userFullName}</p>
+    <p><strong style="color: black;">Username:</strong> ${userUsername}</p>
+    <p><strong style="color: black;">Phone:</strong> ${userPhone}</p>
   `;
 }
 
@@ -75,4 +73,35 @@ function loadItem() {
 }
 window.onload = () => {
   loadItem();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var goTopButton = document.querySelector('.go_top');
+
+    if (goTopButton) {
+        goTopButton.addEventListener('click', function() {
+            scrollToTopSmooth(500);
+        });
+    }
+
+    window.addEventListener('scroll', function() {
+        const scrollPosition = window.scrollY;
+
+        if (scrollPosition > 0) {
+            goTopButton.style.display = 'block';
+        } else {
+            goTopButton.style.display = 'none';
+        }
+    });
+});
+
+function scrollToTopSmooth(duration) {
+    var scrollStep = (-window.scrollY) / (duration / 15),
+        scrollInterval = setInterval(function() {
+            if (window.scrollY !== 0) {
+                window.scrollBy(0, scrollStep);
+            } else {
+                clearInterval(scrollInterval);
+            }
+        }, 15);
 }
